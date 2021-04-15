4f1e422f70072d9c043b61e5cc7d6a16b0aece9a
Client <65496622+tneilc@users.noreply.github.com>
2021-04-15 21:58:43 +0300

    Revert "fix"
    This reverts commit 327a848c314f583d514538e71ac42121b68b278e.

--- DiscordBotEthan/Commands/Say.cs
+++ DiscordBotEthan/Commands/Say.cs
@@ -13,6 +13,7 @@ namespace DiscordBotEthan.Commands {
             if (ctx.Message.MessageType == DSharpPlus.MessageType.Reply) {
                 _ = await new DiscordMessageBuilder()
                     .WithContent(message)
+                    .WithReply(ctx.Message.ReferencedMessage.Id, true)
                     .SendAsync(ctx.Channel);
             } else {
                 await ctx.RespondAsync(message);
